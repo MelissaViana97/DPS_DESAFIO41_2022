2616d3afca0c42c8d46392478f3c32cdede8136b
Eduardo Tejada <eduardotejada_12@hotmail.com>
2022-09-04 00:45:33 -0600

    Update App.js

--- ejercicio1/src/App.js
+++ ejercicio1/src/App.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
-import Lista from './components/Lista';
+import Checklist from './components/Lista';
 
 function App() {
 
   //Declaracion de Estados
-  const [ lista, setLista ] = useState([]); //{ nombre: '', precio: '', cantidad: 1 }
+  const [ Checklist, setChecklist ] = useState([]); 
   const [ total, setTotal ] = useState(0);
 
   useEffect(() => {
-    const calcularTotal = () => {
+    const sumarTotal = () => {
       const subtotal = lista.map( (list) => {
         return list.cantidad * list.precio
       })
@@ -22,13 +22,13 @@ function App() {
   
       setTotal(stotal);
     }
-    calcularTotal();
+    sumarTotal();
   }, [ lista ])
 
   return (
     <div className="container">
-      <Form setLista = { setLista } lista = { lista } />
-      <Lista lista = { lista } setLista = { setLista } />
+      <Form setChecklist = { setChecklist } checklist = { lista } />
+      <Checklist checklist = { checklist} setChecklist = { setLista } />
       <div className = 'total-container'>
         <p>Total: ${ total }</p>
       </div>
